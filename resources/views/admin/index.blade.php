43c5f17d4319ba3af50230549bcebcff96b3af2e
Emmanuel Adiba <eabaagah@gmail.com>
2021-04-04 19:25:27 +0000

    update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -200,12 +200,40 @@
           </li>
           <li class="nav-item">
             <a href="pages/widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-dropdown"></i>
+              <i class="nav-icon fas fa-envelope"></i>
               <p>
-                Add Book
-                <span class="right badge badge-danger">New</span>
+                Requests
               </p>
             </a>
+          </li>
+          <li class="nav-item menu-open">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-plus"></i>
+              <p>
+                Add Item
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Book</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./index2.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Shelve</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./index3.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Cupboard</p>
+                </a>
+              </li>
+            </ul>
           </li>
         </ul>
       </nav>
