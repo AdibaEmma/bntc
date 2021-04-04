6d7334017716521c8fc14eac74aa97eda9b5adcd
Emmanuel Adiba <eabaagah@gmail.com>
2021-04-04 14:40:29 +0000

    admin lockscreen

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,8 @@ Route::post('/admins/login', [AdminLoginController::class, 'store']);
 
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
+Route::get('/confirm-password', [AdminLockscreenController::class, 'index'])->name('admin.lockcreen');
+Route::post('/confirm-password', [AdminLockscreenController::class, 'store'])->name('admin.lockcreen');
 
 Route::get('/students/{user:name}/dashboard', [StudentController::class, 'index'])->name('student.dashboard');
 Route::get('/students/{user:name}/profile', [StudentController::class, 'show'])->name('student.profile');
