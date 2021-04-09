acde39a2977bc025cddaf9c7c93e8b7ec69aa22d
Emmanuel Adiba <eabaagah@gmail.com>
2021-04-09 17:18:54 +0000

    update

--- resources/views/admin/add_book.blade.php
+++ resources/views/admin/add_book.blade.php
@@ -24,7 +24,7 @@
           <div class="row">
             <div class="col-12 col-sm-6">
                 <label for="title">Title</label>
-                <input type="text" name="title" class="form-control" id="title" placeholder="title">
+                <input type="text" name="title" class="form-control mb-3" id="title" placeholder="title">
             </div>
             <div class="col-12 col-sm-6">
                 <label for="title">Author</label>
@@ -54,72 +54,16 @@
             </div>
             <!-- /.col -->
             <div class="col-md-12">
-                <h6>Add image</h6>
-                    <div id="actions" class="row">
-                      <div class="col-lg-6">
-                        <div class="btn-group w-100">
-                          <span class="btn btn-success col fileinput-button">
-                            <i class="fas fa-plus"></i>
-                            <span>Add files</span>
-                          </span>
-                          <button type="submit" class="btn btn-primary col start">
-                            <i class="fas fa-upload"></i>
-                            <span>Start upload</span>
-                          </button>
-                          <button type="reset" class="btn btn-warning col cancel">
-                            <i class="fas fa-times-circle"></i>
-                            <span>Cancel upload</span>
-                          </button>
-                        </div>
-                      </div>
-                      <div class="col-lg-6 d-flex align-items-center">
-                        <div class="fileupload-process w-100">
-                          <div id="total-progress" class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                            <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="table table-striped files" id="previews">
-                      <div id="template" class="row mt-2">
-                        <div class="col-auto">
-                            <span class="preview"><img src="data:," alt="" data-dz-thumbnail /></span>
-                        </div>
-                        <div class="col d-flex align-items-center">
-                            <p class="mb-0">
-                              <span class="lead" data-dz-name></span>
-                              (<span data-dz-size></span>)
-                            </p>
-                            <strong class="error text-danger" data-dz-errormessage></strong>
-                        </div>
-                        <div class="col-4 d-flex align-items-center">
-                            <div class="progress progress-striped active w-100" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                              <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
-                            </div>
-                        </div>
-                        <div class="col-auto d-flex align-items-center">
-                          <div class="btn-group">
-                            <button class="btn btn-primary start">
-                              <i class="fas fa-upload"></i>
-                              <span>Start</span>
-                            </button>
-                            <button data-dz-remove class="btn btn-warning cancel">
-                              <i class="fas fa-times-circle"></i>
-                              <span>Cancel</span>
-                            </button>
-                            <button data-dz-remove class="btn btn-danger delete">
-                              <i class="fas fa-trash"></i>
-                              <span>Delete</span>
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  <!-- /.card-body -->
-                  <div class="card-footer">
-                    Visit <a href="https://www.dropzonejs.com">dropzone.js documentation</a> for more examples and information about the plugin.
-                  </div>
-                <!-- /.card -->
+              <label for="image">Image</label>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">Upload</span>
+                </div>
+                <div class="custom-file">
+                  <input type="file" name="image" class="custom-file-input" id="inputGroupFile01">
+                  <label class="custom-file-label" for="inputGroupFile01">Choose image</label>
+                </div>
+              </div>
               </div>
                     
             <!-- /.col -->
@@ -136,8 +80,7 @@
         </div>
         <!-- /.card-body -->
         <div class="card-footer">
-          Visit <a href="https://select2.github.io/">Select2 documentation</a> for more examples and information about
-          the plugin.
+          Contact <a href="https://adibaemma.github.io/dist">The Developer</a> for more info.
         </div>
       </div>
       <!-- /.card -->
